82847cbd96f5036e9a71ac8a9e19c5fdf9853f02
Marcel <marcelnedza2000@gmail.com>
2023-09-03 11:21:38 +0200

    Backdrop added when error modal shows. Not it is possible to close modal error by click on background

--- src/components/ErrorModal/ErrorModal.js
+++ src/components/ErrorModal/ErrorModal.js
@@ -2,20 +2,24 @@ import Button from "../AddUser/Button";
 import styles from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
+  
   const closeErrorModal = () => {
     props.setIsErrorModalVisible(false);
   };
 
   return (
-    <div className={styles["error-modal"]}>
-      <div className={styles["error-title-container"]}>
-        <p className={styles["error-title"]}>Invalid input</p>
-      </div>
+    <div>
+      <div className={styles.backdrop} onClick={closeErrorModal}></div>
+      <div className={styles["error-modal"]}>
+        <div className={styles["error-title-container"]}>
+          <p className={styles["error-title"]}>Invalid input</p>
+        </div>
 
-      <p className={styles["error-message"]}>{props.error}</p>
+        <p className={styles["error-message"]}>{props.error}</p>
 
-      <div className={styles["error-button-container"]}>
-        <Button onClick={closeErrorModal} buttonName="Okay" />
+        <div className={styles["error-button-container"]}>
+          <Button onClick={closeErrorModal} buttonName="Okay" />
+        </div>
       </div>
     </div>
   );
